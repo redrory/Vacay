e4296dc36f5bf3f9116e01c583551a8b62d411a8
Rory Walker <redrory@gmail.com>
2011-04-10 19:35:35 -0500

    register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
   def show
       @user = User.find(params[:id])
+  end
+    
+  def new
+      @user = User.new
+      @title = "Sign up"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Vacay Tracker"
+      redirect_to @user
+    else
+      render 'new'
     end
+  end
 
 end
