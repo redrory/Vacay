502d2ed607d3048de4265acfb372c10fdbc63396
Rory Walker <redrory@gmail.com>
2011-04-12 21:39:26 -0500

    debug

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,13 +1,4 @@
 class Site < ActiveRecord::Base
 
-  def self.up
-    create_table :employee do |t|
-      t.string :name
-      t.timestamps
-  end
-  
-  def self.down
-    drop_table :employee
-  end
-  
+
 end
