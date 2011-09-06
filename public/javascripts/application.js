e27d69dc008078bafc85559c1bf14462b33ad2be
Rory Walker <redrory@gmail.com>
2011-09-06 07:35:07 -0500

    Calendar start

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+
 jQuery(document).ready(function() { 
+
+
+	jQuery( '#employee_dob' ).datepicker();
+
 	
 	jQuery('a[id = "maxVacay"]').click(function(){
 		
